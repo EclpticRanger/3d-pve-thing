2117eb134a4fffd8e13189989fb661653323a7f0
Joe-sire <josiah@arndt.co.nz>
2025-10-07 10:54:13 +1300

    Update game preload.gd

--- game preload.gd
+++ game preload.gd
@@ -4,5 +4,5 @@ var settings = load("res://Ui/Setings/setings.gd").new()
 
 
 func _ready() -> void:
-	settings.loadsettings()
+	#settings.loadsettings()
 	get_tree().change_scene_to_file("res://Ui/Start Menue/Main.tscn")
